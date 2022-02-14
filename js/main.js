fe71b33eee7fa4231b9c65578221bea841b6aba6
Luc-Ky Khuu <khuu96@gmail.com>
2022-02-14 15:20:34 -0800

    Add the nextEntryId to the object.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 /* global data */
 /* exported data */
+var entryObject = {};
+var entryArray = [];
+var nextEntryId = 0;
 var $photoURL = document.querySelector('#photo');
 var $img = document.querySelector('img');
 $photoURL.addEventListener('input', function (event) {
@@ -9,6 +12,11 @@ $photoURL.addEventListener('input', function (event) {
 var $form = document.querySelector('form');
 document.addEventListener('submit', function (event) {
   event.preventDefault();
+  entryObject.title = $form.elements.title.value;
+  entryObject.photoUrl = $form.elements.photo.value;
+  entryObject.notes = $form.elements.notes.value;
+  entryObject.nextEntryId = nextEntryId;
+  entryArray.push(entryObject);
 
   $form.reset();
 });
