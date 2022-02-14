e7c6f1b4bd74f79f81cb3fbbc1f55824af4843c8
Luc-Ky Khuu <khuu96@gmail.com>
2022-02-14 15:09:35 -0800

    Listen for 'submit' events on the journal entry form

--- js/main.js
+++ js/main.js
@@ -5,3 +5,10 @@ var $img = document.querySelector('img');
 $photoURL.addEventListener('input', function (event) {
   $img.setAttribute('src', $photoURL.value);
 });
+
+var $form = document.querySelector('form');
+document.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  $form.reset();
+});
