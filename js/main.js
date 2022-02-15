1ec77e32ba322c2308f366c9757c4867f1cf76d5
Luc-Ky Khuu <khuu96@gmail.com>
2022-02-15 12:48:12 -0800

    Define a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML.

--- js/main.js
+++ js/main.js
@@ -14,6 +14,8 @@ document.addEventListener('submit', function (event) {
   $img.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
 });
+var $dataView = document.querySelector('ul');
+$dataView.prepend(createNewElement());
 
 function saveEntry() {
   var entryObject = {};
@@ -23,4 +25,32 @@ function saveEntry() {
   entryObject.entryId = data.nextEntryId;
   data.nextEntryId++;
   return entryObject;
+}
+
+function createNewElement(entry) {
+  var spacing = 'b-margin no-top-margin';
+  var createLi = document.createElement('li');
+  var createRowDiv = document.createElement('div');
+  var createImgDiv = document.createElement('div');
+  var createImg = document.createElement('img');
+  var createTextDiv = document.createElement('div');
+  var createTitle = document.createElement('h2');
+  var createNote = document.createElement('p');
+
+  var imgSrc = entry.photoUrl;
+  createImgDiv.appendChild(createImg).setAttribute('src', imgSrc);
+  createRowDiv.appendChild(createImgDiv).setAttribute('class', 'column-half b-margin');
+
+  var title = entry.title;
+  createTitle.textContent = title;
+  var note = entry.notes;
+  createNote.textContent = note;
+  createTextDiv.appendChild(createTitle).setAttribute('class', spacing);
+  createTextDiv.appendChild(createNote).setAttribute('class', spacing);
+  createRowDiv.appendChild(createTextDiv).setAttribute('class', 'column-half');
+
+  createLi.appendChild(createRowDiv).setAttribute('class', 'row');
+  createLi.setAttribute('class', 'b-margin');
+
+  return createLi;
 }
